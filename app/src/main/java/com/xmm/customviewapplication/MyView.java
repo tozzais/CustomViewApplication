2a697701d67cfdedf817a2804413530615ddccbf
tozzais <835683840@qq.com>
2017-06-13 11:03:46 +0800

    2017 01 13

--- app/src/main/java/com/xmm/customviewapplication/MyView.java
+++ app/src/main/java/com/xmm/customviewapplication/MyView.java
@@ -1,7 +1,11 @@
 package com.xmm.customviewapplication;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -9,18 +13,40 @@ import android.view.View;
  */
 
 public class MyView extends View {
+
+    Paint mPaint;
+
     public MyView(Context context) {
+
         super(context);
+
+        init();
     }
+
+
 
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public MyView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+
     }
 
+    private void init() {
+
+        mPaint = new Paint();
+        mPaint.setColor(Color.RED);
+    }
+
+    /**
+     * 负责View的测量
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         setMeasuredDimension(measure(widthMeasureSpec),measure(heightMeasureSpec));
@@ -31,6 +57,7 @@ public class MyView extends View {
         int specMode = MeasureSpec.getMode(measureSpec);
         int specSize = MeasureSpec.getSize(measureSpec);
         if(specMode == MeasureSpec.EXACTLY){
+            //EXACTLY是指定高度  或则是match_parent
             result = specSize;
         }else{
             //不是EXACTLY（指定高度的时候 需要给默认值）
@@ -40,7 +67,21 @@ public class MyView extends View {
                 result = Math.min(result,specSize);
             }
         }
+
+        //还有一种是 想多大就多大。 MeasureSpec.UNSPECIFIED
         return result;
     }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        /**
+         * getX 相对于屏幕远点的位置  本文中是 32
+         * getY同理 也是32
+         */
+       Log.e("-------",getX()+":"+getY()+":"+getWidth()+":"+getHeight());
+       canvas.drawLine(0,0,getX()+getHeight(),getY()+getWidth(),mPaint);
+
+    }
 }

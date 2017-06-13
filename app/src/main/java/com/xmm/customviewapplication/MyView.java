a38a4436381d54da2165b12dd89678400c11db7b
tozzais <835683840@qq.com>
2017-06-13 17:15:04 +0800

    001

--- app/src/main/java/com/xmm/customviewapplication/MyView.java
+++ app/src/main/java/com/xmm/customviewapplication/MyView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -15,6 +16,7 @@ import android.view.View;
 public class MyView extends View {
 
     Paint mPaint;
+    Rect mRect;
 
     public MyView(Context context) {
 
@@ -40,6 +42,7 @@ public class MyView extends View {
 
         mPaint = new Paint();
         mPaint.setColor(Color.RED);
+        mPaint.setStrokeWidth(5);
     }
 
     /**
@@ -77,11 +80,29 @@ public class MyView extends View {
         super.onDraw(canvas);
 
         /**
-         * getX 相对于屏幕远点的位置  本文中是 32
+         * getX view距离父容器左边缘的距离
+         * 当一个组件没有PaddingLeft的时候  是一样的。有PaddingLeft的时候 就是这个值
          * getY同理 也是32
+         *
+         *  1.在构造方法中无论是getWidth还是getMeasuredWidth都是得不到正确数值的。
+         *  2.getMeasuredWidth得到正确数值需要在调用过onMeasure之后。
+         *  3.getWidth得到正确数值需要调用过onLayout之后。
+         *
          */
-       Log.e("-------",getX()+":"+getY()+":"+getWidth()+":"+getHeight());
-       canvas.drawLine(0,0,getX()+getHeight(),getY()+getWidth(),mPaint);
+        Log.e("-------",getX()+":"+getY()+":"+getWidth()+":"+getHeight()+":"+getMeasuredWidth());
+        //绘制直线  一块画布的对角线
+//        canvas.drawLine(0,0,getWidth(),getHeight(),mPaint);
+        //绘制 一条水平的直线 距离组件的顶部 10个像素
+        canvas.drawLine(0,10,getWidth(),10,mPaint);
+
+        /**
+         * Rect 和 RectF 都是用来表示坐标系中的一个区域
+         * Rect的参数是int， RectF的参数是 Float(更加的准确一点)
+         */
+        //绘制矩形
+        mRect = new Rect();
+        canvas.drawRect(mRect,mPaint);
+
 
     }
 }

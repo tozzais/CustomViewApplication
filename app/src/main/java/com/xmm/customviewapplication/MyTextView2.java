59178b57eb1a3f33f50e59cab0878c000df3984c
tozzais <835683840@qq.com>
2017-06-14 11:37:51 +0800

    添加TextView2 实现TextView闪动的效果。来自《ANdroid群英传》

--- app/src/main/java/com/xmm/customviewapplication/MyTextView2.java
+++ app/src/main/java/com/xmm/customviewapplication/MyTextView2.java
@@ -3,7 +3,10 @@ package com.xmm.customviewapplication;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
@@ -13,52 +16,64 @@ import android.widget.TextView;
 
 public class MyTextView2 extends TextView {
 
-    private Paint mPaint1,mPaint2;
+
+
+
     public MyTextView2(Context context) {
         super(context);
-        init();
     }
 
 
     public MyTextView2(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
     }
 
     public MyTextView2(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
     }
 
 
-    private void init() {
 
-        mPaint1 = new Paint();
-        mPaint1.setColor(Color.RED);
-        mPaint1.setStyle(Paint.Style.FILL);
-        mPaint2 = new Paint();
-        mPaint2.setColor(Color.YELLOW);
-        mPaint2.setStyle(Paint.Style.FILL);
-
-//        mPaint1.setStrokeWidth(10);
-    }
 
 
     @Override
     public void draw(Canvas canvas) {
-        canvas.drawRect(0,0,getMeasuredWidth(),getMeasuredHeight(),mPaint1);
-        canvas.drawRect(10,10,getMeasuredWidth()-10,getMeasuredHeight()-10,mPaint2);
-
-        canvas.save();
-
+//        canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint1);
+//        canvas.drawRect(10, 10, getMeasuredWidth() - 10, getMeasuredHeight() - 10, mPaint2);
+//        canvas.save();
 //        canvas.translate(20,0);
-
-
         super.draw(canvas);
+//        canvas.restore();
 
-        canvas.restore();
+        if(mGradientMatrix != null){
+            mTranslate += mViewWidth/5;
+            if(mTranslate > 2*mViewWidth){
+                mTranslate = -mViewWidth;
+            }
+            mGradientMatrix.setTranslate(mTranslate,0);
+            mLinearGradient.setLocalMatrix(mGradientMatrix);
+            postInvalidateDelayed(100);
+        }
     }
 
 
+    private int mViewWidth;
+    private int mTranslate;
+    private LinearGradient mLinearGradient;
+    private Matrix mGradientMatrix;
+    private Paint mPaint;
 
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (mViewWidth == 0) {
+            mViewWidth = getMeasuredWidth();
+            if(mViewWidth > 0){
+                mPaint = getPaint();
+                mLinearGradient = new LinearGradient(0, 0, mViewWidth, 0, new int[]{Color.BLUE, 0xffffffff, Color.BLUE}, null, Shader.TileMode.CLAMP);
+                mPaint.setShader(mLinearGradient);
+                mGradientMatrix = new Matrix();
+            }
+        }
+    }
 }
